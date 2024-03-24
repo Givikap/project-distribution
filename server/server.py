0f57843a78495b7e402325a15e338bf78dc0813e
Givikap <kaitosekiya@outlook.com>
2024-03-23 21:01:53 -0500

    Redo last push

--- server/server.py
+++ server/server.py
@@ -36,7 +36,7 @@ def get_plot(subpath, filename):
 
 @app.route("/api/plot/<semester>/<course_name>/<course_number>", methods=["GET"])
 def plot(semester, course_name, course_number):
-    plot_path = f"plots/{semester}/{course_name.lower()}/{course_number}/"
+    plot_path = f"plots/{semester}/{course_name}/{course_number}/"
 
     if not os.path.exists(plot_path):
         df = pd.read_csv(f"csv/{semester}.csv")
