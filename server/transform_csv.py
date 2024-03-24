0d52c4fed3a77c2234e47b5a818beada54942a85
Givikap <kaitosekiya@outlook.com>
2024-03-24 08:32:04 -0500

    Update database and backend, I'm tired

--- server/transform_csv.py
+++ server/transform_csv.py
@@ -15,14 +15,23 @@ def main(school):
     os.makedirs(f"csv/{school}/", exist_ok=True)
 
     for file in glob.glob("to_transform/*"):
-        df = pd.read_csv(file)
+        df = pd.read_csv(file, encoding_errors="ignore")
 
-        new_df = df.filter(items=["Subject", "Course", "Primary Instructor", "F", "W"]).rename(columns={"Subject": "CRS SUBJ CD", "Course": "CRS NBR"})
-
+        new_df = df.rename(columns={
+            "Subject": "CRS SUBJ CD", 
+            "Course": "CRS NBR", 
+            "Course Subject": "CRS SUBJ CD", 
+            "Course Number": "CRS NBR"
+            }).filter(items=["CRS SUBJ CD", "CRS NBR", "Primary Instructor", "F", "W"])
+    
         for letter in ["A", "B", "C", "D"]:
-            new_df[letter] = df.filter(regex=f"^{letter}.?$").sum(axis=1)
+            new_df[letter] = df.filter(regex=f"^{letter}.?$").sum(axis=1).astype(int)
 
-        new_df = new_df.reindex(columns=["CRS SUBJ CD", "CRS NBR", "A", "B", "C", "D", "F", "W", "Primary Instructor"])
+        new_df = new_df.reindex(columns=["CRS SUBJ CD", "CRS NBR", "A", "B", "C", "D", "F", "W", "Primary Instructor"]).dropna(subset=["A", "B", "C", "D", "F", "W"])
+
+        new_df["F"] = new_df["F"].astype(int)
+        new_df["W"] = new_df["W"].astype(int)
+
         new_df.to_csv(f"csv/{school}/{file.split('/')[1]}", index=False)
 
 
